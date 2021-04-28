7e279c6e5d7c8ad95367433f2413e3fb98740bee
Alexander Liu <liujalexander84@gmail.com>
2021-04-27 21:35:02 -0700

    rewrote the whole thing

--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,50 @@
 export type ColorSchemes = "light" | "dark";
-export type ColorSchemeOptions = "light" | "dark" | "system";
+export type ColorSchemeSettings = "light" | "dark" | "system";
 export const ColorSchemeStorageKey = "__darkmode-helper_mode__";
 
 /**
  * @returns `dark` if system prefers dark mode, `light` if system prefers
  *          light mode
  */
-export function querySystemColorScheme(): ColorSchemes {
+export function getSystemPreferredColorScheme(): ColorSchemes {
    return matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+}
+
+/**
+ * sets the color scheme to specified setting and applies it
+ * @param scheme color scheme to set it to, either `light`, `dark`, or `system`
+ */
+export function setColorScheme(scheme: ColorSchemeSettings): void {
+   scheme === "system"
+      // system scheme is when no item in localstorage
+      ? localStorage.removeItem(ColorSchemeStorageKey)
+      // light/dark store in localstorage
+      : localStorage.setItem(ColorSchemeStorageKey, scheme);
+
+   initColorScheme();
 }
 
 /**
  * gets color scheme setting
  */
-export function getColorScheme(): ColorSchemeOptions {
-   const storedmode = localStorage.getItem(ColorSchemeStorageKey);
-
-   // no key is system
-   if (storedmode === null) return "system";
-
-   // validate its a valid mode and return it
-   if (storedmode === "dark" || storedmode === "light") return storedmode;
-
-   // invalid mode, nuke it lol and use system
-   localStorage.removeItem(ColorSchemeStorageKey);
+export function getColorSchemeSetting(): ColorSchemeSettings {
+   const stored = localStorage.getItem(ColorSchemeStorageKey);
+   if (stored === "dark" || stored === "light") return stored;
+   // its null or some other random string
+   // no nuke, just leave it i guess lul
    return "system";
 }
 
 /**
- * sets color scheme setting to one of `dark`, `light`, or `system`
- */
-export function setColorScheme(mode: ColorSchemeOptions): void {
-   mode === "dark" || mode === "light"
-      // dark mode or light mode, set it in storage
-      ? localStorage.setItem(ColorSchemeStorageKey, mode)
-      // not in storage is system
-      : localStorage.removeItem(ColorSchemeStorageKey);
-
-   mode === "dark" || (mode !== "light" && querySystemColorScheme() === "dark")
-      ? document.documentElement.classList.add("dark")
-      : document.documentElement.classList.remove("dark");
-}
-
-/**
- * initialises the color scheme. If setting is set to `dark`, or it is set to `system`
- * and the system prefers dark mode, then it adds the class to the body. Oherwise
- * do nothing (light mode).
+ * initialises the color scheme. pulls the setting from local storage
+ * and applies it
  */
 export function initColorScheme(): void {
-   setColorScheme(getColorScheme());
+   const scheme = getColorSchemeSetting();
+
+   useDarkMode()
+      ? document.documentElement.classList.add("dark")
+      : document.documentElement.classList.remove("dark");
 }
 
 /**
@@ -60,8 +56,8 @@ export function initColorScheme(): void {
  *
  * @returns mode that it set to
  */
-export function toggleDisplayMode(): ColorSchemeOptions {
-   const mode = getColorScheme();
+export function toggleDisplayMode(): ColorSchemeSettings {
+   const mode = getColorSchemeSetting();
    const newmode = mode === "system" ? "dark"
                  : mode === "dark" ? "light"
                  : "system"
@@ -70,38 +66,61 @@ export function toggleDisplayMode(): ColorSchemeOptions {
 }
 
 /**
- * @returns if the setting is set to `dark` mode
+ * @returns true if the setting is set to `dark`
  */
-export function setToDarkMode() {
-   return getColorScheme() === "dark";
+export function settingIsDarkMode(): boolean {
+   return getColorSchemeSetting() === "dark";
 }
 
 /**
- * @returns if the setting is set to `light` mode
+ * @returns if the setting is set to `light`
  */
-export function setToLightMode() {
-   return getColorScheme() === "light";
+export function settingIsLightMode(): boolean {
+   return getColorSchemeSetting() === "light";
 }
 
 /**
- * @returns if the setting is set to `system` mode
+ * @returns if the setting is set to `system`
  */
-export function setToSystemMode() {
-   return getColorScheme() === "system";
+export function settingIsSystemMode(): boolean {
+   return getColorSchemeSetting() === "system";
+}
+
+/**
+ * sets to dark mode
+ */
+export function setToDarkMode(): void {
+   setColorScheme("dark");
+}
+
+/**
+ * sets to light mode
+ */
+export function setToLightMode(): void {
+   setColorScheme("light");
+}
+
+/**
+ * sets to system preferred mode
+ */
+export function setToSystemMode(): void {
+   setColorScheme("system");
 }
 
 /**
  * @returns if system should use dark mode (either setting is `dark` or
  *          setting is `system` and system prefers dark mode)
  */
-export function isDarkMode() {
-   return setToDarkMode() || querySystemColorScheme() === "dark";
+export function shouldUseDarkMode(): boolean {
+   const scheme = getColorSchemeSetting();
+   return scheme === "dark" || (scheme === "system" && getSystemPreferredColorScheme() === "dark");
 }
 
 /**
  * @returns if system should use light mode (either setting is `light` or
  *          setting is `system` and system prefers light mode)
  */
-export function isLightMode() {
-   return setToLightMode() || querySystemColorScheme() === "light";
+export function shouldUseLightMode(): boolean {
+   const scheme = getColorSchemeSetting();
+   return scheme === "light" || (scheme === "system" && getSystemPreferredColorScheme() === "light");
 }
