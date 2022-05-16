113976b9351cac2876c8ac80cc3cb5dde65c1f64
Autumn Meadow <blazeykirin@gmail.com>
2022-05-16 02:01:43 -0700

    actually export these options lol

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,5 @@ export {
 	get_system_preference
 } from "./helpers"
 export { create_local_storage } from "./store-methods/local-storage";
+export { create_custom } from "./store-methods/custom";
+export { create_custom_async } from "./store-methods/custom-async";
