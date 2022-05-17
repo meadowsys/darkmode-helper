13157fc3ce54336c5a43d304869fc88c499f19e5
Autumn Meadow <blazeykirin@gmail.com>
2022-05-17 00:00:03 -0700

    add some watch api for localStorage

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,5 +1,5 @@
 /** one instance of matcher, don't create a new one every time (probably microoptimisation) */
-const matcher_prefers_dark = matchMedia("(prefers-color-scheme: dark)");
+export const matcher_prefers_dark = matchMedia("(prefers-color-scheme: dark)");
 
 /**
  * either light or dark, represents the actual colour schemes
@@ -19,6 +19,7 @@ export type ColourSchemeSetting =
 	| "system";
 
 export type WatchCallback = (s: ColourScheme) => void;
+export type MediaQueryCallback = (e: MediaQueryListEvent) => void;
 
 export type DarkModeHelper<S extends "sync" | "async", W extends boolean = false> = {
 	/** gets the mode that should be used, either "light" or "dark" */
@@ -62,4 +63,51 @@ export function get_system_preference(): ColourScheme {
 
 export function setting_to_mode(s: ColourSchemeSetting) {
 	return s === "system" ? get_system_preference() : s;
+}
+
+export function create_matcher_event_listener_sync(
+	get_setting: DarkModeHelper<"sync">["get_setting"],
+	watch_cb: WatchCallback
+): MediaQueryCallback {
+	return (event: MediaQueryListEvent) => watch_cb(setting_to_mode(get_setting()));
+}
+
+export function create_matcher_event_listener_async(
+	get_setting: DarkModeHelper<"async">["get_setting"],
+	watch_cb: WatchCallback
+): MediaQueryCallback {
+	return (event: MediaQueryListEvent) => get_setting()
+		.then(setting_to_mode)
+		.then(watch_cb);
+}
+
+export function create_event_listener_store<S extends "sync" | "async">(
+	type: S,
+	get_setting: DarkModeHelper<S>["get_setting"]
+) {
+	let stored_fns: Array<[
+		WatchCallback,
+		MediaQueryCallback,
+		string?
+	]> = [];
+
+	return { store_function, get_function };
+
+	function store_function(watch_cb: WatchCallback, key?: string): MediaQueryCallback {
+		let media_query_cb = type === "sync"
+			? create_matcher_event_listener_sync(get_setting as any, watch_cb)
+			: create_matcher_event_listener_async(get_setting as any, watch_cb);
+
+		key
+			? stored_fns.push([watch_cb, media_query_cb, key])
+			: stored_fns.push([watch_cb, media_query_cb]);
+
+		return media_query_cb;
+	}
+
+	function get_function(fn: WatchCallback | string) {
+		return typeof fn === "string"
+			? stored_fns.find(sf => fn === sf[2])?.[1]
+			: stored_fns.find(sf => fn === sf[0])?.[1];
+	}
 }
--- src/store-methods/local-storage.ts
+++ src/store-methods/local-storage.ts
@@ -1,8 +1,8 @@
 import * as h from "../helpers";
 
 /** this helper uses `localStorage` as the method of storing the preferences. */
-export function create_local_storage(store_key: string): h.DarkModeHelper<"sync"> {
-	return { get, get_setting, set };
+export function create_local_storage(store_key: string): h.DarkModeHelper<"sync", true> {
+	let event_listener_store = h.create_event_listener_store("sync", get_setting);
 
 	function get_setting(): h.ColourSchemeSetting {
 		return (localStorage.getItem(store_key) || "system") as h.ColourSchemeSetting;
@@ -16,4 +16,21 @@ export function create_local_storage(store_key: string): h.DarkModeHelper<"sync"
 	function set(s: h.ColourSchemeSetting) {
 		localStorage.setItem(store_key, s);
 	}
+
+	const watch: h.DarkModeHelper<"sync", true>["watch"] = (arg1, arg2 = undefined) => {
+		if (typeof arg1 === "function") {
+			let listener = event_listener_store.store_function(arg1);
+			h.matcher_prefers_dark.addEventListener("change", listener);
+		} else if (typeof arg1 === "string" && typeof arg2 === "function") {
+			let listener = event_listener_store.store_function(arg2 as any, arg1);
+			h.matcher_prefers_dark.addEventListener("change", listener);
+		}
+	}
+
+	function unwatch(key: h.WatchCallback | string) {
+		let fn = event_listener_store.get_function(key);
+		fn && h.matcher_prefers_dark.removeEventListener("change", fn);
+	}
+
+	return { get, get_setting, set, watch, unwatch };
 }
