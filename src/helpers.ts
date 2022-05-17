4c2809f1ca4019f813c6887301a7b4defbcc9cfa
Autumn Meadow <blazeykirin@gmail.com>
2022-05-17 16:10:20 -0700

    create again!

---
+++ src/helpers.ts
@@ -0,0 +1,69 @@
+/** one instance of matcher, don't create a new one every time (probably microoptimisation) */
+export const matcher_prefers_dark = matchMedia("(prefers-color-scheme: dark)");
+
+/**
+ * either light or dark, represents the actual colour schemes
+ * that can be presented on the UI
+ */
+export type ColourScheme =
+	| "light"
+	| "dark";
+
+/**
+ * either light, dark, or system, represents the settings that are stored.
+ * system means that light/dark is picked based on the system preference.
+ */
+export type ColourSchemeSetting =
+	| "light"
+	| "dark"
+	| "system";
+
+export type WatchCallback = (s: ColourScheme) => void;
+export type MediaQueryCallback = (e: MediaQueryListEvent) => void;
+
+export type SettingStore = {
+	get_setting: () => Promise<ColourSchemeSetting>;
+	set_setting: (s: ColourSchemeSetting) => Promise<void>;
+};
+
+/** queries the system for its preferred colour scheme, either "light" or "dark" */
+export function get_system_preference(): ColourScheme {
+	return matcher_prefers_dark.matches ? "dark" : "light";
+}
+
+
+/**
+ * takes a setting and turns it into a mode, by transforming "system" setting
+ * into "light" or "dark" based on system preference
+ */
+function setting_to_mode(s: ColourSchemeSetting) {
+	return s === "system" ? get_system_preference() : s;
+}
+
+export type ColourSchemeModeAndSetting = {
+	mode: ColourScheme;
+	setting: ColourSchemeSetting;
+};
+
+export function create_settings_cache(cb: (c: ColourSchemeModeAndSetting) => void) {
+	let setting: ColourSchemeSetting = "system";
+	let mode: ColourScheme = setting_to_mode(setting);
+
+	function get(): ColourSchemeModeAndSetting {
+		return { setting, mode };
+	}
+
+	function set(s: ColourSchemeSetting): ColourSchemeModeAndSetting {
+		setting = s;
+		return update();
+	}
+
+	function update(): ColourSchemeModeAndSetting {
+		mode = setting_to_mode(setting);
+		return get();
+	}
+
+	matcher_prefers_dark.addEventListener("change", () => cb(update()));
+
+	return { get, set, update };
+}
---
+++ src/index.ts
@@ -0,0 +1,61 @@
+import {
+	ColourScheme,
+	ColourSchemeSetting,
+	ColourSchemeModeAndSetting,
+	SettingStore,
+	WatchCallback,
+	create_settings_cache
+} from "./helpers";
+
+export async function define_darkmode_helper(
+	_settings_store: SettingStore | Promise<SettingStore>
+) {
+	let watch_cbs: Array<WatchCallback> = [];
+	const cached_setting = create_settings_cache(
+		({ mode }) => watch_cbs.forEach(cb => cb(mode))
+	);
+	const settings_store = await Promise.resolve(_settings_store);
+	cached_setting.set(await settings_store.get_setting());
+
+	async function get_setting_and_mode(bypass = false): Promise<ColourSchemeModeAndSetting> {
+		if (bypass) return cached_setting.set(await settings_store.get_setting());
+		return cached_setting.get();
+	}
+
+	async function get(bypass = false): Promise<ColourScheme> {
+		return (await get_setting_and_mode(bypass)).mode;
+	}
+
+	async function get_setting(bypass = false): Promise<ColourSchemeSetting> {
+		return (await get_setting_and_mode(bypass)).setting;
+	}
+
+	async function set_setting(setting: ColourSchemeSetting): Promise<void> {
+		await settings_store.set_setting(setting);
+		const mode_and_setting = cached_setting.set(setting);
+		watch_cbs.forEach(cb => cb(mode_and_setting.mode));
+	}
+
+	function watch(cb: WatchCallback) {
+		get().then(cb);
+		watch_cbs.push(cb);
+	}
+
+	function unwatch(cb: WatchCallback) {
+		const i = watch_cbs.findIndex(c => c === cb);
+		i >= 0 && watch_cbs.splice(i, 1);
+	}
+
+	const helper = {
+		get,
+		get_setting,
+		get_setting_and_mode,
+		set_setting,
+		watch,
+		unwatch
+	};
+
+	return function use_darkmode_helper() {
+		return helper;
+	}
+}
